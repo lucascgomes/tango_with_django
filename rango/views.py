3c21244cd93195b797e4a99516d2f6f61efd4387
Lucas <lucgms@gmail.com>
2014-05-08 16:55:14 -0700

    Part 12 - Bing API
    - Exercises not done

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from datetime import datetime
-
+from rango.bing_search import run_query
 
 def decode_url(category_name_url):
     return category_name_url.replace('_', ' ')
@@ -198,6 +198,18 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/rango/')
 
+def search(request):
+    context = RequestContext(request)
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render_to_response('rango/search.html', {'result_list': result_list}, context)
 
 
 
